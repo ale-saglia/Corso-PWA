979693c2db458795b3cc93fa1eb65f2d92aa2a02
Alessandro Saglia <ale.saglia@gmail.com>
2019-10-03 18:32:53 +0200

    Done 9.6

---
+++ Esercitazioni/Esercitazione 09/Es06.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <title>Esercizio 9.6</title>
+        <meta name="author" content="Alessandro Saglia">
+        <style type="text/css">p.err {color:red}</style>
+    </head>
+
+    <body>
+        <h1>Esercitazione 9.6</h1>
+        <?php
+            if(isset($_REQUEST['testo'])){
+                $regex = '/^(?:[\w]+[\.\,\!\?\:\;\s]\s?)+[\w]+[\.\,\!\?\:\;]?$/';
+                $testo = trim($_REQUEST['testo']);
+
+                if(!preg_match($regex, $testo))
+                    echo "<p class=\"err\">Errore - testo non valido: inserire solo parole separate da punteggiatura e spazi</p>";
+                else{
+                    $ris = preg_split("/[\W\s]/", $testo, NULL, PREG_SPLIT_NO_EMPTY);
+                    foreach($ris as $value){
+                        if(!isset($parole[$value]))
+                            $parole[$value] = 1;
+                        else
+                            ++$parole[$value];
+                    }
+
+                    if(count($ris) == 0)
+                        echo "<p class=\"err\">Nessuna parola inserita</p>\n";
+                    else{
+                        echo "<p>Numero di parole univoche: ".count($parole)."</p>";
+                        echo "<ul>";
+                        foreach($parole as $key=>$value)
+                            echo "<li>\"$key\" - numero di occorrenze: $value</li>";
+                        echo "</ul>";
+                    }
+                }    
+            }
+            else
+                echo "<p class=\"err\">Errore - dati mancanti</p>\n";
+        ?>
+    </body>
+</html>
