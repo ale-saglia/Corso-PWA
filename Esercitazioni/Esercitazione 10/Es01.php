04955d5f25b6a9e7dc0e05517f14f1fa1b65a022
Alessandro Saglia <ale.saglia@gmail.com>
2019-10-21 21:52:29 +0200

    Es01 completed

---
+++ Esercitazioni/Esercitazione 10/Es01.php
@@ -0,0 +1,17 @@
+<?php
+    setcookie("Country", "IT");
+?>
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <title>Esercizio 10.1</title>
+        <meta name="author" content="Alessandro Saglia">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>Esercizio 10.1</h1>
+        <p>Italia!</p>
+        <p><a href="Es01bis.php">Pagina successiva</a></p>
+    </body>
+</html>
---
+++ Esercitazioni/Esercitazione 10/Es01bis.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <title>Esercizio 10.1</title>
+        <meta name="author" content="Alessandro Saglia">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>Esercizio 10.1</h1>
+        <?php
+            if(isset($_COOKIE["Country"])){
+                $nazione = $_COOKIE["Country"];
+                echo"<p>Il valore del cookie COUNTRY &egrave; $nazione </p>";
+            }
+            else{
+                echo"<p class='err'>ERRORE: Cookie \"Country\" assente</p>";
+                echo"<p><a href='lab10_1a.php'>Pagina precedente</a></p>";
+            }
+        ?>
+    </body>
+</html>
